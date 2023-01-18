f426388025316a4c447458cb1486f78eb3364278
MARSADMANZOOR8 <120159246+MARSADMANZOOR8@users.noreply.github.com>
2023-01-18 22:51:42 +0500

    Add files via upload

--- task07.cpp
+++ task07.cpp
@@ -1,97 +1,51 @@
-#include <iostream>
-using namespace std ;
-float cost(string city , string product , int quantity);
+#include<iostream>
+using namespace std;
 main()
 {
-    string city;
-    cout << " enter the name of the city :" << endl;
-    cin >> city;
-    string product;
-    cout << " enter the name of the product :" << endl;
-    cin >> product;
-    int quantity;
-    cout << " enter the number of products :" << endl;
-    cin >> quantity;
-    float result ;
-    result = cost(city , product ,quantity);
-    cout << " the total amount the you have to give : " << result;
-}
-float cost(string city , string product , int quantity)
-{
-    float total;
-   if (city=="sofia" && product =="coffee")
-   {
-    total = 0.50 * quantity;
-    return total;
-   }
-  else if (city=="sofia" && product =="water")
-   {
-    total = 0.80 * quantity;
-    return total;
-   }
-   else if (city=="sofia" && product =="beer")
-   {
-    total = 1.20 * quantity;
-    return total;
-   }
-   else if (city=="sofia" && product =="sweets")
-   {
-    total = 1.45 * quantity;
-    return total;
-   }
-    else if (city=="sofia" && product =="peanuts")
-   {
-    total = 1.60 * quantity;
-    return total;
-   }
-    else if (city=="piovdiv" && product =="coffee")
-   {
-    total = 0.40 * quantity;
-    return total;
-   }
-    else if (city=="piovdiv" && product =="water")
-   {
-    total = 0.70 * quantity;
-    return total;
-   }
-    else if (city=="piovdiv" && product =="beer")
-   {
-    total = 1.15 * quantity;
-    return total;
-   }
-   else if (city=="piovdiv" && product =="sweets")
-   {
-    total = 1.30 * quantity;
-    return total;
-   }
-   else if (city=="piovdic" && product =="peanuts")
-   {
-    total = 1.50 * quantity;
-    return total;
-   }
-   else if (city=="varna" && product =="coffee")
-   {
-    total = 0.45 * quantity;
-    return total;
-   }
-    else if (city=="varna" && product =="water")
-   {
-    total = 0.70 * quantity;
-    return total;
-   }
-    else if (city=="varna" && product =="beer")
-   {
-    total = 1.10 * quantity;
-    return total;
-   }
-    else if (city=="varna" && product =="sweets")
-   {
-    total = 1.35 * quantity;
-    return total;
-   }
-    else if (city=="varna" && product =="peanuts")
-   {
-    total = 1.55 * quantity;
-    return total;
-   }
-}
+	
+    int start_hours;
+    cout << "enter the hours when the exam starts : " ;
+    cin >> start_hours;
+    int start_minutes;
+    cout << "enter the minutes when the exam starts : " ;
+    cin >> start_minutes;
+    int arrival_hours;
+    cout << "enter the hours when the student arrived : " ;
+    cin >> arrival_hours;
+    int arrival_minutes;
+    cout << "enter the minutes when the student arrived : " ;
+    cin >> arrival_minutes;
+    int start_time;
+    start_time = (start_hours*60) + start_minutes ;
+    int arrival_time ;
+    arrival_time = (arrival_hours*60) + arrival_minutes ;
+    int difference;
+    difference = arrival_time-start_time ;
+    int hours=0;
+
+	if (difference<-59)
+	    {
+	    	difference = difference + 60;
+	    	hours = hours + 1;
+	    	cout << hours << ":" << (-1)*difference << "before the start of the exam " << endl;
+		
+	   }
+	   else
+	   {
+	   	cout <<(-1)* difference << " minutes " << "before the start of the exam " << endl ; 
+	   	
+	   }
+	   	if (difference>59)
+	    {
+	    	difference = difference - 60;
+	    	hours = hours + 1;
+	    	cout << hours << ":" << difference << "after the start of the exam " << endl;
+		
+	   }
+	   else
+	   {
+	   	cout << difference << " minutes " << "after the start of the exam " << endl ; 
+	   	
+	   }
+	
+	}
